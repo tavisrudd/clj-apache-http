4fcbaa000b3ec56cbedf6ddf33609838a523e13a
Tavis Rudd <tavis@damnsimple.com>
2012-01-13 11:44:00 -0800

    update apache deps (to avoid clash when used in project that also uses clj-http)

--- project.clj
+++ project.clj
@@ -2,7 +2,7 @@
  :description "Clojure HTTP library using the Apache HttpClient."
  :dependencies [[org.clojure/clojure "1.3.0"]
                 [org.clojure/data.json "0.1.2"]
-                [org.apache.httpcomponents/httpcore "4.0.1"]
-                [org.apache.httpcomponents/httpmime "4.0.1"]
+                [org.apache.httpcomponents/httpcore "4.1.2"]
+                [org.apache.httpcomponents/httpmime "4.1.2"]
                 [commons-logging/commons-logging "1.1.1"]
-                [org.apache.httpcomponents/httpclient "4.0.1"]])
+                [org.apache.httpcomponents/httpclient "4.1.2"]])
